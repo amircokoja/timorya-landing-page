b1b3f5545c2bac51a961d4ec3c974e9e544ca0ec
Amir Cokoja <amircoka@gmail.com>
2025-08-03 18:34:22 +0200

    Folder restrucutred

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { PainPoints } from "./components/pain-points";
-import { Header } from "./components/header";
-import { Hero } from "./components/hero";
-import { Feature } from "./components/feature";
-import { Pricing } from "./components/pricing";
-import { FAQ } from "./components/faq/faq";
-import { Footer } from "./components/footer";
-import { ContactForm } from "./components/contact-form";
+import { PainPoints } from "../components/pain-points";
+import { Header } from "../components/header";
+import { Hero } from "../components/hero";
+import { Feature } from "../components/feature";
+import { Pricing } from "../components/pricing";
+import { FAQ } from "../components/faq/faq";
+import { Footer } from "../components/footer";
+import { ContactForm } from "../components/contact-form";
 
 export default function Home() {
   return (
